5277647217ea299a6f6a265af43e31ce2743e7bc
nagaratna_a <nagaratna_a@BLL05588.persistent.co.in>
2017-06-15 10:57:04 +0530

    updated

---
+++ src/searchstring.java
@@ -0,0 +1,53 @@
+import java.util.HashMap;
+
+import org.yaml.snakeyaml.events.MappingEndEvent;
+
+public class searchstring {
+
+	public static void main(String args[], String DOMAIN){
+		
+	HashMap<String,String> has = new HashMap<String,String> (); 
+	String file=null;
+	String DOMAIN1=null;
+	String CLASS=null;
+	String INSTANCE=null;
+	String ZOBJDATE=null;
+	String ZOBJTIME=null;
+	String ZOBJRCRC=null;
+	String ZOBJID=null;
+	String ZUNUSED1=null;
+	String ZOBJCID=null;
+	String ZOBJRSIZ=null;
+	String ZSTOP000=null;
+	String ZSTOP001=null;
+	String ZSTOP002=null;
+	String ZSTOP999=null;
+	String ZSVCNAME=null;
+	String ZSVCTTYP=null;
+	String ZSVCMO=null;
+	String ZSVCCSTA=null;
+	String ZSVCPRI=null;
+	String ALWAYS_=null;
+	String _ALWAYS_=null;
+	
+	
+	
+	has.put(file="PRIMARY",DOMAIN1="SOFTWARE");
+//	String s2 = null;
+//	String []strArray=s.split(" ");
+	
+	for(int i=0; i<has;i++) {
+	    if(strArray[i].equals("walk")){
+	    	
+	    	s2=strArray[i].toString();
+	    	
+	    	System.out.println("right ");
+		    System.out.println("walk  " +s2+i++);
+	    	
+		   //break;	
+	    }
+
+	}
+	}
+	
+}
