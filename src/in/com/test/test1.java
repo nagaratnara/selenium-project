7c7bcbd5701e478d877b60ff5f6ab68d5db4065c
nagaratna_a <nagaratna_a@BLL05588.persistent.co.in>
2017-06-15 11:07:25 +0530

    updated this project

---
+++ src/in/com/test/test1.java
@@ -0,0 +1,25 @@
+package in.com.test;
+
+import java.io.IOException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+
+public class test1 {
+	public static void main(String[] args) throws InvalidFormatException, IOException {
+		
+//		//Workbook wb=WorkbookFactory.create(new File ("D:\\EclipseLunaAutomationFolder\\test\\Book1.xlsx"));
+//		//org.apache.poi.ss.usermodel.Sheet s=wb.createSheet("Sheet1");
+//		String[][] tabArray=null;
+//
+//        //Workbook workbook = Workbook.getWorkbook(new File("D:\\EclipseLunaAutomationFolder\\test\\Book1.xlsx"));
+//        Workbook wb=WorkbookFactory.create(new File ("D:\\EclipseLunaAutomationFolder\\test\\Book1.xlsx"));
+//        Sheet sheet = workbook.getSheet(sheetName);
+//        int startRow,startCol, endRow, endCol,ci,cj;
+//        Cell tableStart=sheet.findCell(tableName);
+//        startRow=tableStart.getRow();
+//        startCol=tableStart.getColumn();
+		
+		
+	}
+
+}
---
+++ src/removearrayduplicates.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+
+public class removearrayduplicates {
+
+	public static void main(String[] args) {
+
+		//consider we converted array to arraylist
+		
+		//int a[] ={2,4,5,6,3,3,2};
+		
+//		DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
+//		caps.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,true);
+		//caps.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS,true);
+		System.setProperty("webdriver.chrome.driver", "D:\\EclipseLunaAutomationFolder\\test\\IEDriverServer.exe");
+		WebDriver driver = new ChromeDriver();
+		//driver.get("www.google.com");
+		
+	
+					
+		
+		
+		
+		
+		
+	}
+
+}
